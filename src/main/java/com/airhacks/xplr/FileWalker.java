732760dabe24812770aa3a7d1a4b09032794ce9f
Adam Bien <abien@adam-bien.com>
2020-01-25 17:47:45 +0100

    works with files and folders

--- src/main/java/com/airhacks/xplr/FileWalker.java
+++ src/main/java/com/airhacks/xplr/FileWalker.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -16,6 +17,9 @@ import java.util.List;
 public interface FileWalker {
 
     public static List<Path> findJars(Path root) {
+        if (isJar(root)) {
+            return Arrays.asList(root);
+        }
         return scanFolder(root);
     }
 
